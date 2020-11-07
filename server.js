0ab52df695a937d91538412f3d0899565e4be08c
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-11-07 01:45:34 +0000

    lvl 1 completed

--- server.js
+++ server.js
@@ -8,11 +8,43 @@ app.use(cors());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
+let nextId = bookings.length;
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+app.get("/bookings", function(request, response) {
+  response.send(bookings)
+});
+
+app.get("/bookings/:id", function(request, response) {
+  const selectedBooking = bookings.find(item => request.params.id == item.id);
+
+  if(selectedBooking)
+    response.send(selectedBooking);
+  else
+    response.sendStatus(404);
+});
+
+app.post("/bookings", function(request, response) {
+  let newBooking = request.body;
+  newBooking.id = ++nextId;
+
+  bookings.push(newBooking);
+  response.status(201).send(newBooking);
+});
+
+app.delete("/bookings/:id", function(request, response) {
+  const selectedBooking = bookings.find(item => request.params.id == item.id);
+
+  if(selectedBooking) {
+    bookings.splice(selectedBooking, 1);
+    response.sendStatus(204);
+  }   
+  else
+    response.sendStatus(404);
+});
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
